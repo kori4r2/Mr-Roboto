091b97bf1fa8c2ce5afa442ec813d9b60036e61a
Italo Tobler <xitsilvax@hotmail.com>
2018-04-23 16:48:20 +0900

    added comments

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+# Import bot token from environment variables
 from os import environ
 botToken = environ.get("MR_ROBOTO_TOKEN")
 
+# Create updater and save reference to dispatcher
 from telegram.ext import Updater
 updater = Updater(token=botToken)
-
 dispatcher = updater.dispatcher;
 
+# Set up logging errors
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# Function to run with the start command
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="beep boop")
 
+# Add command handler for start command
 from telegram.ext import CommandHandler
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
 
+# Start polling commands
 updater.start_polling()
