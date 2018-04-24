54be280903782138705af5ded695eee6e90e060b
Italo Tobler <xitsilvax@hotmail.com>
2018-04-24 12:50:22 +0900

    testing an aditional command

--- app.py
+++ app.py
@@ -15,10 +15,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="beep boop")
 
-# Add command handler for start command
+# Function to run with the new command
+def new(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="new beep boop")
+
 from telegram.ext import CommandHandler
+# Add command handler for start command
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
+
+# Add command handler for new command
+new_handler = CommandHandler("new", new)
+dispatcher.add_handler(new_handler)
 
 # Start polling commands
 updater.start_polling()
